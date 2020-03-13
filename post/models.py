b2f88dac957c37a5bbf3abe350eb6603a7d8404f
saiwingwong <wong_saiwing@yahoo.com>
2020-03-13 00:06:22 +0000

    update 'post/post-list' main page

--- post/models.py
+++ post/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib import timezone
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -15,7 +15,7 @@ class Post(models.Model):
 
     ## latest posts on top
     class Meta:
-        ordering = ('created_time')
+        ordering = ('-created_time', )
 
     def __str__(self):
         return self.title
--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'post'
 
 urlpatterns = [
-    
+    path('post-list/', views.post_list, name="post_list"),
 ]
