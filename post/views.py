5c122941a79afcbd8af3b2e05665c035b6abfccf
saiwingwong <wong_saiwing@yahoo.com>
2020-03-13 03:57:25 +0000

    basic Bootstrap home page

--- post/views.py
+++ post/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def post_list(request):
-    return HttpResponse("Hello Blog!")
+    posts = Post.objects.all()
+    context = { 'posts': posts }
+    return render(request, 'post/list.html', context)
