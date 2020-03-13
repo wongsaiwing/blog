8aaa35bec6cb7355586e8dc592779c9dde5e1b62
saiwingwong <wong_saiwing@yahoo.com>
2020-03-13 20:20:45 +0000

    optimise post linking

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,5 @@ app_name = 'post'
 
 urlpatterns = [
     path('post-list/', views.post_list, name="post_list"),
+    path('post-list/<int:id>/', views.post_detail, name='post_detail'),
 ]
--- post/views.py
+++ post/views.py
@@ -6,3 +6,9 @@ def post_list(request):
     posts = Post.objects.all()
     context = { 'posts': posts }
     return render(request, 'post/list.html', context)
+
+def post_detail(request, id):
+    ## id = Primary Key
+    post = Post.objects.get(id=id)
+    context = {'post':post}
+    return render(request, 'post/detail.html', context)
