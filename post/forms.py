2c3a002e117b1cc01bfe3dd275e0b4d646fd77ff
saiwingwong <wong_saiwing@yahoo.com>
2020-03-15 19:31:19 +0000

    PublishPost && EditPost function implementation

---
+++ post/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Post
+
+# allow users publish posts using forms
+class PostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ('title', 'body')
--- post/views.py
+++ post/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import PostForm
 from .models import Post
+from django.contrib.auth.models import User
+
 import markdown
 # Create your views here.
 
@@ -20,3 +24,54 @@ def post_detail(request, id):
 
     context = {'post':post}
     return render(request, 'post/detail.html', context)
+
+def post_create(request):
+    # determine whether the user want to publish a post
+    if request.method == "POST":
+        post_form = PostForm(data=request.POST)
+
+        if post_form.is_valid():
+            new_post = post_form.save(commit=False)
+            new_post.author = User.objects.get(id=1)
+            new_post.save()
+            return redirect("post:post_list")
+        else:
+            return HttpResponse("Invalid content, please edit again")
+
+    # if the user not going to publish a post
+    else:
+        post_form=PostForm()
+        context = {'post_post': post_form}
+
+        return render(request, 'post/create.html', context)
+
+# def post_delete(request, id):
+#     # delete post by its id
+#     # then back to the post list
+#     post = Post.objects.get(id=id)
+#     post.delete()
+#     return redirect("post:post_list")
+
+    # update a post
+def post_update(request, id):
+        # using POST 
+        # enter oroginal form by GET 
+        # redirect to the modified psot 
+
+    post=Post.objects.get(id=id)
+
+    if request.method == "POST":
+        post_form = PostForm(data=request.POST)
+
+        if post_form.is_valid():
+            # overwrite the original context
+            post.title = request.POST['title']
+            post.body = request.POST['body']
+            post.save()
+            return redirect("post:post_detail", id=id)
+        else:
+            return HttpResponse("Invalid content, please edit again")
+    else:
+        post_form = PostForm()
+        context = {'post': post, 'post_form': post_form}
+        return render(request, 'post/update.html', context)
