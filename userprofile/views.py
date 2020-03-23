fff19838c54bc560b76cd51dea3a078c004b074f
saiwingwong <wong_saiwing@yahoo.com>
2020-03-23 16:49:07 +0800

    user registration

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 
 # Create your views here.
 
 def user_login(request):
     if request.method == 'POST':
-        user_login_form = UserLoginForm(data=request.POST)
-        if user_login_form.is_valid():
-            data = user_login_form.cleaned_data
+        user_register_form = UserRegisterForm(data=request.POST)
+        if user_register_form.is_valid():
+            data = user_register_form.cleaned_data
             
             # check whether username/password matches
             user = authenticate(username=data['username'], password=data['password'])
@@ -32,3 +32,28 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("post:post_list")
+
+# user registration
+def user_register(request):
+    if request.method == 'POST':
+        user_register_form = UserRegisterForm(data=request.POST)
+
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+
+            # set password
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            login(request, new_user)
+            return redirect("post:post_list")
+
+        else:
+            return HttpResponse("Invalid form, please retry")
+
+    elif request.method == 'GET':
+        user_register_form = UserRegisterForm()
+        context = { 'form':user_register_form }
+        return render(request, 'userprofile/register.html', context)
+
+    else:
+        return HttpResponse("POST or GET")
